90e961a34261a2394c6d901ee7cb48f58ee99225
Chamkaur <manpreetsinghgarewal@gmail.com>
2017-09-22 15:56:37 +0530

    update cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,7 +15,7 @@ class Cors
      */
     public function handle($request, Closure $next)
     {   
-        $domains =['http://localhost:4200','http://localhost:9999','http://182.75.232.100:8126','http://photos2.hwphotopoints.org.uk'];
+        $domains =['http://localhost:4200','http://photos2.hwphotopoints.org.uk'];
         if(isset($request->server()['HTTP_ORIGIN'])){
             $origin = $request->server()['HTTP_ORIGIN'];
             
@@ -23,6 +23,8 @@ class Cors
                 header('Access-Control-Allow-Origin: '.$origin);
                 //header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization,X-Auth-Token');
                 header('Access-Control-Allow-Headers: Content-Type, x-xsrf-token, x_csrftoken,token,Authorization');
+            }else{
+                return response()->json(["You can't access it"], 404); 
             }
 
         }
