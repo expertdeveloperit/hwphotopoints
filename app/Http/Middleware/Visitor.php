f17a725779cb3b9c07a9f6147918881e8356f546
Chamkaur <manpreetsinghgarewal@gmail.com>
2017-10-06 17:41:48 +0530

    update routes and admin area

---
+++ app/Http/Middleware/Visitor.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+class Visitor
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request,Closure $next)
+    {   
+        if(Auth::user()->role = 3){
+            return response()->json(['error' => "Your can't access this area.",'status'=>false]);
+        }
+
+        return $next($request);
+    }
+}
